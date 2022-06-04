78df01028bb58fd0f1f02256e5ef0a5c5d5fc177
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-06-05 08:41:14 +0900

    Add: TODOの値にボールを追加して、リストの横にボールを配置した

---
+++ lib/parts/image_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ImageButton extends StatelessWidget {
+  const ImageButton({
+    required this.isSelected,
+    required this.image,
+    required this.onTap,
+    Key? key,
+  }) : super(key: key);
+
+  final bool isSelected;
+  final Widget image;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        color: isSelected ? Colors.green : Colors.transparent,
+        child: image,
+      ),
+    );
+  }
+}
